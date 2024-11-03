12050aeb028f5be0614cd8067edf2448be185733
Manarhassanin <96919829+Manarhassanin@users.noreply.github.com>
2024-11-03 14:55:10 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %matplotlib inline
+import warnings
+warnings.filterwarnings(action='ignore')
+data = pd.read_csv('healthcare-dataset-stroke-data.csv')
+# print(data.head())
+print(data.shape)
+print(data.isnull().sum())
